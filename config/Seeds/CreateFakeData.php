a6678c054df4562752d81c0fefcce2f37b6e9efb
Geoff Oliver <geoff@plan8studios.com>
2020-02-03 21:07:02 -0700

    added data for settings into seeder

--- config/Seeds/CreateFakeData.php
+++ config/Seeds/CreateFakeData.php
@@ -20,9 +20,8 @@ class CreateFakeData extends AbstractSeed
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
         $userId = Text::uuid();
-        $now = $faker->dateTime()->format('Y-m-d H:i:s');
+        $now = date('Y-m-d H:i:s');
         $password = (new DefaultPasswordHasher)->hash('password');
 
         $users = [[
@@ -35,80 +34,67 @@ class CreateFakeData extends AbstractSeed
         ]];
 
         $this->table('users')->insert($users)->save();
-        /*
-        $posts = [];
-        $medias = [];
-        $comments = [];
 
-        for ($i = 0; $i < 10; $i++) {
-            $postId = Text::uuid();
-            $now = $faker->dateTime()->format('Y-m-d H:i:s');
-            $post = [
-                'id' => $postId,
-                'name' => $faker->words(5, true),
-                'content' => '<p>' . implode('</p><p>', $faker->paragraphs(4)) . '</p>',
-                'public' => true,
-                'allow_comments' => true,
-                'user_id' => $userId,
-                'created' => $now,
-                'modified' => $now
-            ];
-
-            if ($i % 2) {
-                $mediaId = Text::uuid();
-                $now = $faker->dateTime()->format('Y-m-d H:i:s');
-                $medias[]= [
-                    'id' => $mediaId,
-                    'post_id' => $postId,
-                    'mime' => 'image/jpeg',
-                    'square_thumbnail' => '2019/07/13/dog-thumbnail.jpg',
-                    'thumbnail' => '2019/07/13/dog-thumbnail.jpg',
-                    'local_filename' => '2019/07/13/dog.jpg',
-                    'original_filename' => 'a-really-cool-dog.jpg',
-                    'user_id' => $userId,
-                    'created' => $now,
-                    'modified' => $now
-                ];
-
-                $maxComments = rand(1, 20);
-                for ($n = 0; $n < $maxComments; $n++) {
-                  $now = $faker->dateTime()->format('Y-m-d H:i:s');
-                  $comments[]= [
-                    'id' => Text::uuid(),
-                    'model_id' => $mediaId,
-                    'comment' => '<p>' . implode('</p><p>', $faker->paragraphs(rand(1, 4))) . '</p>',
-                    'approved' => true,
-                    'public' => true,
-                    'posted_by' => $faker->email,
-                    'display_name' => $faker->name,
-                    'created' => $now,
-                    'modified' => $now
-                  ];
-                }
-            }
-
-            $maxComments = rand(1, 20);
-            for ($n = 0; $n < $maxComments; $n++) {
-              $now = $faker->dateTime()->format('Y-m-d H:i:s');
-              $comments[]= [
+        // add some settings
+        $settings = [
+            [
                 'id' => Text::uuid(),
-                'model_id' => $postId,
-                'comment' => '<p>' . implode('</p><p>', $faker->paragraphs(rand(1, 4))) . '</p>',
-                'approved' => true,
-                'public' => true,
-                'posted_by' => $faker->email,
-                'display_name' => $faker->name,
                 'created' => $now,
-                'modified' => $now
-              ];
-            }
+                'modified' => $now,
+                'name' => 'about-page',
+                'value' => ''
+            ],
+            [
+                'id' => Text::uuid(),
+                'created' => $now,
+                'modified' => $now,
+                'name' => 'homepage-about',
+                'value' => __('Say something about yourself here!')
+            ],
+            [
+                'id' => Text::uuid(),
+                'created' => $now,
+                'modified' => $now,
+                'name' => 'cover-title',
+                'value' => __('Welcome to my homepage')
+            ],
+            [
+                'id' => Text::uuid(),
+                'created' => $now,
+                'modified' => $now,
+                'name' => 'cover-subtitle',
+                'value' => __('These are things I\'ve said and done.')
+            ],
+            [
+                'id' => Text::uuid(),
+                'created' => $now,
+                'modified' => $now,
+                'name' => 'timezone',
+                'value' => 'America/New_York'
+            ],
+            [
+                'id' => Text::uuid(),
+                'created' => $now,
+                'modified' => $now,
+                'name' => 'site-name',
+                'value' => __('My Homepage'),
+            ],
+            [
+                'id' => Text::uuid(),
+                'created' => $now,
+                'modified' => $now,
+                'name' => 'site-title',
+                'value' => __('Just another site on the internet.'),
+            ],
+            [
+                'id' => Text::uuid(),
+                'created' => $now,
+                'modified' => $now,
+                'name' => 'time-format',
+                'value' => 'F j, Y \a\t g:i a'
+            ]
+        ];
 
-            $posts[]= $post;
-        }
-
-        $this->table('posts')->insert($posts)->save();
-        $this->table('medias')->insert($medias)->save();
-        $this->table('comments')->insert($comments)->save();
-        */
+        $this->table('settings')->insert($settings)->save();
     }
 }
