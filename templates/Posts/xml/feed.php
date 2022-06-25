72dc840a3305e2ffdc2806f776ae0b3871fb8a86
Geoff Oliver <geoff@plan8studios.com>
2022-06-25 16:25:51 -0600

    more feed shit

--- templates/Posts/xml/feed.php
+++ templates/Posts/xml/feed.php
@@ -6,7 +6,11 @@ $Parsedown = new Parsedown();
 $Parsedown->setStrictMode(true);
 
 ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss
+    version="2.0"
+    xmlns:atom="http://www.w3.org/2005/Atom"
+    xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+>
     <channel>
         <title><?= Hash::get($settings, 'site-name'); ?></title>
         <description><?= Hash::get($settings, 'site-name'); ?></description>
@@ -22,6 +26,10 @@ $Parsedown->setStrictMode(true);
             <link><?= $this->Url->build(['_name' => 'viewPost', $post->id], ['fullBase' => true]); ?></link>
             <pubDate><?= $post->modified->format('D, d M Y h:i:s O'); ?></pubDate>
             <description><![CDATA[<?= $Parsedown->text($post->content); ?>]]></description>
+            <?php if($post->allow_comments): ?>
+            <comments><?= $this->Url->build(['_name' => 'viewPost', $post->id], ['fullBase' => true]); ?>#comments</comments>
+            <slash:comments><?= count($post->comments); ?></slash:comments>
+            <?php endif; ?>
         </item>
     <?php endforeach; ?>
     </channel>
