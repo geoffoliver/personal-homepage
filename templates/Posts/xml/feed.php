3e28d2330d6672de65b36c9e0d1a63a43b9ab761
Geoff Oliver <geoff@plan8studios.com>
2022-06-25 15:43:13 -0600

    fuck

--- templates/Posts/xml/feed.php
+++ templates/Posts/xml/feed.php
@@ -19,7 +19,8 @@ $Parsedown->setStrictMode(true);
         <name><?= Hash::get($settings, 'site-name'); ?></name>
     </author>
 <?php foreach ($posts as $post): ?>
-    <entry id="<?= $this->Url->build(['_name' => 'viewPost', $post->id], ['fullBase' => true]); ?>">
+    <entry>
+        <id><?= $this->Url->build(['_name' => 'viewPost', $post->id], ['fullBase' => true]); ?></id>
         <title type="html"><?= $post->name ?? $post->created->setTimezone(Hash::get($settings, 'timezone'))->format(Hash::get($settings, 'time-format')); ?></title>
         <link href="<?= $this->Url->build(['_name' => 'viewPost', $post->id], ['fullBase' => true]); ?>" />
         <updated><?= $post->modified->format('c'); ?></updated>
