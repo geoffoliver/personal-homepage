46be77a3a3641d753eda111d5d79e77aae1dc10d
Geoff Oliver <geoff@plan8studios.com>
2022-06-25 16:14:06 -0600

    more feed shit

--- templates/Posts/xml/feed.php
+++ templates/Posts/xml/feed.php
@@ -10,16 +10,16 @@ $Parsedown->setStrictMode(true);
     <channel>
         <title><?= Hash::get($settings, 'site-name'); ?></title>
         <description><?= Hash::get($settings, 'site-name'); ?></description>
-        <link href="<?= $this->Url->build('/', ['fullBase' => true]); ?>" />
+        <link><?= $this->Url->build('/', ['fullBase' => true]); ?></link>
     <?php if ($posts): ?>
-        <lastBuildDate><?= $posts->first()->modified->format('c'); ?></lastBuildDate>
+        <lastBuildDate><?= $posts->first()->modified->format('D, d M Y G:i:s T'); ?></lastBuildDate>
     <?php endif; ?>
     <?php foreach ($posts as $post): ?>
         <item>
             <guid><?= $this->Url->build(['_name' => 'viewPost', $post->id], ['fullBase' => true]); ?></guid>
             <title><?= $post->name ?? $post->created->setTimezone(Hash::get($settings, 'timezone'))->format(Hash::get($settings, 'time-format')); ?></title>
             <link><?= $this->Url->build(['_name' => 'viewPost', $post->id], ['fullBase' => true]); ?></link>
-            <pubDate><?= $post->modified->format('c'); ?></pubDate>
+            <pubDate><?= $post->modified->format('D, d M Y G:i:s T'); ?></pubDate>
             <description><![CDATA[<?= $Parsedown->text($post->content); ?>]]></description>
         <?php if($post->source): ?>
             <source url="<?= urlencode($post->source); ?>"><![CDATA[<?= $post->source; ?>]]></source>
